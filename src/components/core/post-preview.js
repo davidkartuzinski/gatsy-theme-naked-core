04528e3d6b9b77ec3b4ed6506670512bc54df73d
davidkartuzinski <david@kaidawei.me>
2022-08-31 15:07:27 +0200

    blog post snippets now showing on the Blog page, however, images are still not. Individual blog pages are not showing either.

---
+++ src/components/core/post-preview.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+// import { GatsbyImage } from 'gatsby-plugin-image';
+
+import { ReadNextIcon, PublishDateIcon } from './icons';
+
+const PostPreview = ({
+  slug,
+  image,
+  imageAlt,
+  title,
+  date,
+  figcaption,
+  excerpt,
+}) => (
+  <>
+    <Link to={'/blog/' + slug}>
+      <h2>{title}</h2>
+    </Link>
+    <figure>
+      <Link to={'/blog/' + slug}>
+        {/* <Image fluid={image} alt={imageAlt} /> */} Image here
+      </Link>
+      <figcaption>{figcaption}</figcaption>
+    </figure>
+    <PublishDateIcon /> Published on <p>{date}</p>
+    <p>{excerpt}</p>
+    <Link to={'/blog/' + slug} aria-label={`Read the rest: ${title} `}>
+      Read the rest <ReadNextIcon />
+    </Link>
+    <hr />
+  </>
+);
+
+export default PostPreview;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import Layout from '../components/structure/layout';
 import Aside from '../components/structure/aside';
 import Seo from '../components/core/Seo';
 import { graphql } from 'gatsby';
+import PostPreview from '../components/core/post-preview';
 
 const BlogRollPage = ({ data }) => {
   return (
@@ -13,9 +14,21 @@ const BlogRollPage = ({ data }) => {
             <h1>Blog Posts</h1>
           </header>
           <ul>
-            {data.blogPosts.nodes.map((node) => (
-              <li key={node.name}>{node.name}</li>
-            ))}
+            {data.blogPosts.nodes.map((node) => {
+              return (
+                <article key={node.id} className='inner-article'>
+                  <PostPreview
+                    slug={node.frontmatter.slug}
+                    // image={node.frontmatter.image.childImageSharp.fluid}
+                    // imageAlt={node.frontmatter.imageAlt}
+                    figcaption={node.frontmatter.imageFigcaption}
+                    title={node.frontmatter.title}
+                    date={node.frontmatter.date}
+                    excerpt={node.frontmatter.description}
+                  />
+                </article>
+              );
+            })}
           </ul>
         </article>
       </main>
@@ -28,9 +41,25 @@ export const Head = () => <Seo title='Blog Posts' slug='blog' />;
 
 export const query = graphql`
   query blogPosts {
-    blogPosts: allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    mdx {
+      id
+    }
+    blogPosts: allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        id
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          slug
+          title
+          author
+          categories
+          description
+          image
+          imageAlt
+          imageFigcaption
+          tags
+        }
+        excerpt(pruneLength: 250)
       }
     }
   }
