5504e0bb2cdd4682b579b1f8e5dbc5d109c81898
davidkartuzinski <david@kaidawei.me>
2022-09-04 02:55:05 +0200

    added mailchimp plugin support

--- src/components/structure/aside.js
+++ src/components/structure/aside.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import Bio from '../widgets/bio';
+import MailChimpSignUp from '../widgets/mailchimp-sign-up';
 
 const Aside = ({ children }) => {
   return (
     <>
       <aside className='page-aside'>
         {children}
+
+        <MailChimpSignUp />
         <Bio />
       </aside>
     </>
